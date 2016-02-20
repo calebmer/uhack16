a560fcff2b2cc4324247aa8c9d6c141459e043ed
calebmer <calebmeredith8@gmail.com>
2016-02-20 15:10:13 -0500

    feat: add some app code

--- add/App.elm
+++ add/App.elm
@@ -1,6 +1,8 @@
 module App where
 
-import Html exposing (div, text)
+import Html exposing (Html, text, div, form, label, textarea, input, button)
+import Html.Attributes exposing (action, value, type', checked)
+import Html.Events exposing (onSubmit, on, targetValue, targetChecked)
 import StartApp.Simple as StartApp
 
 app =
@@ -11,10 +13,49 @@ app =
 
 main = app
 
-model : Bool
-model = True
+type alias Model = 
+  { text : String
+  , important : Bool }
 
+model : Model
+model =
+  { text = ""
+  , important = False }
+
+view : Signal.Address Action -> Model -> Html
 view address model =
-  div [] [text "Hello world"]
+  form
+    [ onSubmit address AddThought
+    , action "javascript:void(0);" ]
+    [ div
+        []
+        [ label [] [ text "Thought" ] ]
+        , textarea
+          [ value model.text
+          , on "input" targetValue (\text -> Signal.message address (UpdateText text)) ]
+          []
+    , div
+        []
+        [ label
+            []
+            [ input
+                [ type' "checkbox"
+                , checked model.important
+                , on "input" targetChecked (\important -> Signal.message address (UpdateImportant important)) ]
+                []
+            , text " Important" ] ]
+    , button
+        []
+        [ text "Add" ] ]
 
-update address model = True
+type Action =
+    UpdateText String
+  | UpdateImportant Bool
+  | AddThought
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    UpdateText text -> { model | text = text }
+    UpdateImportant important -> { model | important = important }
+    AddThought -> model
